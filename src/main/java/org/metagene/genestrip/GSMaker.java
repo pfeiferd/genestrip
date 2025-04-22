664a39eebd156bf84e7464b27401c12c08af6a0e
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2025-04-22 11:44:02 +0200

    Additional tests.

--- src/main/java/org/metagene/genestrip/GSMaker.java
+++ src/main/java/org/metagene/genestrip/GSMaker.java
@@ -1,26 +1,26 @@
 /*
- * 
+ *
  * “Commons Clause” License Condition v1.0
- * 
- * The Software is provided to you by the Licensor under the License, 
+ *
+ * The Software is provided to you by the Licensor under the License,
  * as defined below, subject to the following condition.
- * 
- * Without limiting other conditions in the License, the grant of rights under the License 
+ *
+ * Without limiting other conditions in the License, the grant of rights under the License
  * will not include, and the License does not grant to you, the right to Sell the Software.
- * 
- * For purposes of the foregoing, “Sell” means practicing any or all of the rights granted 
- * to you under the License to provide to third parties, for a fee or other consideration 
- * (including without limitation fees for hosting or consulting/ support services related to 
- * the Software), a product or service whose value derives, entirely or substantially, from the 
- * functionality of the Software. Any license notice or attribution required by the License 
+ *
+ * For purposes of the foregoing, “Sell” means practicing any or all of the rights granted
+ * to you under the License to provide to third parties, for a fee or other consideration
+ * (including without limitation fees for hosting or consulting/ support services related to
+ * the Software), a product or service whose value derives, entirely or substantially, from the
+ * functionality of the Software. Any license notice or attribution required by the License
  * must also include this Commons Clause License Condition notice.
- * 
+ *
  * Software: genestrip
- * 
+ *
  * License: Apache 2.0
- * 
+ *
  * Licensor: Daniel Pfeifer (daniel.pfeifer@progotec.de)
- * 
+ *
  */
 package org.metagene.genestrip;
 
@@ -328,10 +328,10 @@ public class GSMaker extends Maker<GSProject> {
                 refSeqFnaFilesGoal, additionalFastasGoal, accessCollGoal, filledDBGoal, tempDbInfoGoal, projectSetupGoal);
         registerGoal(updateDBGoal);
 
-        // We weed the storeTempDBGoal as a dependency here so that it does not get
-        // automatically deleted until
-        // the final database got stored.
-        StoreDBGoal storeDBGoal = new StoreDBGoal(project, GSGoalKey.DB, project.getDBFile(), updateDBGoal, projectSetupGoal);
+        // We weed the storeTempDBGoal and tempDbInfoGoal as an explicit dependency here so that associated intermediate files get
+        // automatically deleted when final database got stored.
+        StoreDBGoal storeDBGoal = new StoreDBGoal(project, GSGoalKey.DB, project.getDBFile(), updateDBGoal,
+                projectSetupGoal, storeTempDBGoal, tempDbInfoGoal);
         registerGoal(storeDBGoal);
 
         LoadDBGoal loadDBGoal = new LoadDBGoal(project, updateDBGoal, storeDBGoal);
