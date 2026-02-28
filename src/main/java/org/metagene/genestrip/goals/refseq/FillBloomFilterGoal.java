4e3468ac19369f504fd0219127de679bfdb4069e
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2026-02-28 07:49:26 +0100

    Back to old was of sizing DB, HyperLogLog as an experimental choice...

--- src/main/java/org/metagene/genestrip/goals/refseq/FillBloomFilterGoal.java
+++ src/main/java/org/metagene/genestrip/goals/refseq/FillBloomFilterGoal.java
@@ -1,26 +1,26 @@
 /*
- * 
+ *
  * “Commons Clause” License Condition v1.0
- * 
- * The Software is provided to you by the Licensor under the License, 
+ *
+ * The Software is provided to you by the Licensor under the License,
  * as defined below, subject to the following condition.
- * 
- * Without limiting other conditions in the License, the grant of rights under the License 
+ *
+ * Without limiting other conditions in the License, the grant of rights under the License
  * will not include, and the License does not grant to you, the right to Sell the Software.
- * 
- * For purposes of the foregoing, “Sell” means practicing any or all of the rights granted 
- * to you under the License to provide to third parties, for a fee or other consideration 
- * (including without limitation fees for hosting or consulting/ support services related to 
- * the Software), a product or service whose value derives, entirely or substantially, from the 
- * functionality of the Software. Any license notice or attribution required by the License 
+ *
+ * For purposes of the foregoing, “Sell” means practicing any or all of the rights granted
+ * to you under the License to provide to third parties, for a fee or other consideration
+ * (including without limitation fees for hosting or consulting/ support services related to
+ * the Software), a product or service whose value derives, entirely or substantially, from the
+ * functionality of the Software. Any license notice or attribution required by the License
  * must also include this Commons Clause License Condition notice.
- * 
+ *
  * Software: genestrip
- * 
+ *
  * License: Apache 2.0
- * 
+ *
  * Licensor: Daniel Pfeifer (daniel.pfeifer@progotec.de)
- * 
+ *
  */
 package org.metagene.genestrip.goals.refseq;
 
@@ -84,7 +84,16 @@ public class FillBloomFilterGoal<P extends GSProject> extends FastaReaderGoal<Lo
             filter.ensureExpectedSize(sizeGoal.get(), false);
             logHeapInfo();
             readFastas();
-            set(filter.getEntries());
+            long entries = filter.getEntries();
+            // We have to account for the missing entries in the bloom filter due to
+            // inherent FPP. The formula from below works really well,
+            // so we can allow for a low FPP for the bloom filter from 'bloomFilterGoal'
+            // and save memory during db construction.
+            // It is a conservative estimate too, since collisions occur in the process
+            // of filling (as opposed to the FPP formula that considers a filled
+            // bloom filter).
+            entries = (long) (entries / (1d - doubleConfigValue(GSConfigKey.TEMP_BLOOM_FILTER_FPP)));
+            set(entries);
         } catch (IOException e) {
             throw new RuntimeException(e);
         } finally {
