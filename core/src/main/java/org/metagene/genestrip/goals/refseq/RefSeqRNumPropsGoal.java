a952668cd047323357da39bce0f372e082f89f25
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2026-06-05 11:54:54 +0200

    Automating correctness test via KU.

---
+++ core/src/main/java/org/metagene/genestrip/goals/refseq/RefSeqRNumPropsGoal.java
@@ -0,0 +1,56 @@
+/*
+ *
+ * “Commons Clause” License Condition v1.0
+ *
+ * The Software is provided to you by the Licensor under the License,
+ * as defined below, subject to the following condition.
+ *
+ * Without limiting other conditions in the License, the grant of rights under the License
+ * will not include, and the License does not grant to you, the right to Sell the Software.
+ *
+ * For purposes of the foregoing, “Sell” means practicing any or all of the rights granted
+ * to you under the License to provide to third parties, for a fee or other consideration
+ * (including without limitation fees for hosting or consulting/ support services related to
+ * the Software), a product or service whose value derives, entirely or substantially, from the
+ * functionality of the Software. Any license notice or attribution required by the License
+ * must also include this Commons Clause License Condition notice.
+ *
+ * Software: genestrip
+ *
+ * License: Apache 2.0
+ *
+ * Licensor: Daniel Pfeifer (daniel.pfeifer@progotec.de)
+ *
+ */
+package org.metagene.genestrip.goals.refseq;
+
+import org.metagene.genestrip.GSGoalKey;
+import org.metagene.genestrip.GSProject;
+import org.metagene.genestrip.make.FileListGoal;
+import org.metagene.genestrip.make.Goal;
+import org.metagene.genestrip.make.GoalKey;
+import org.metagene.genestrip.make.ObjectGoal;
+import org.metagene.genestrip.store.Database;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class RefSeqRNumPropsGoal<P extends GSProject> extends FileListGoal<P> {
+    public static final String RELEASE_PROPS_FILE_NAME = "release.properties";
+
+    @SafeVarargs
+    public RefSeqRNumPropsGoal(P project, Goal<P>... deps) {
+        super(project, GSGoalKey.REFSEQ_PROP, new File(project.getCommon().getRefSeqDir(), RELEASE_PROPS_FILE_NAME), deps);
+    }
+
+    @Override
+    protected void makeFile(File storeFile) throws IOException {
+        String release = getProject().getAdditionalProperty(GSProject.REFSEQ_RELEASE);
+        Properties props = new Properties();
+        props.setProperty(GSProject.REFSEQ_RELEASE, release);
+        props.store(new FileWriter(storeFile), null);
+    }
+}
